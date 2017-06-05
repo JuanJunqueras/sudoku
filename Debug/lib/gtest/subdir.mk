23766aff2fd217e9247c215669042140b1e0a584
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2017-06-05 15:34:20 -0300

    Modifica cosas de eclipse que me rompia los huevos

--- Debug/lib/gtest/subdir.mk
+++ Debug/lib/gtest/subdir.mk
@@ -17,7 +17,7 @@ OBJS += \
 lib/gtest/%.o: ../lib/gtest/%.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
